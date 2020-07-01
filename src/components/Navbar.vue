e70e4a9376b48b0a4abf954506374b093f2b5cc2
Andrew Blair <blair.d.andrew@gmail.com>
2020-07-01 19:41:27 +0100

    title and button started

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,41 +1,43 @@
 <template>
-    <div class="navbar">
-        <nav class="nav-extended blue lighten-1">
-            <div class="nav-content">
-                <router-link to="">
-                    <span class="nav-title">Inanimate Faces</span>
-                </router-link>
-            </div>
-        </nav>
-        <div class="nav-add-btn deep-orange lighten-1">
-                <router-link to="">
-                    <span class="add-btn">Add a Face</span>
-                    <a class="waves-effect waves-light btn">
-                        <i class="material-icons left">add</i>button
-                    </a>
-                </router-link>
-        </div>
+  <div class="navbar">
+    <nav class="nav-extended blue lighten-1">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Inanimate Faces</span>
+        </router-link>
+      </div>
+    </nav>
+    <div class="nav-add-btn teal lighten-2">
+      <router-link to="">
+        <span class="add-btn"></span>
+        <a class="waves-effect waves-light btn-small">
+          <i class="material-icons left">add</i>Add a Face
+        </a>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar',
-    data(){
-        return {
-
-        }
-    }
-}
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-.navbar nav{
-    position: relative;
-    padding: 0 20px;
+.navbar nav {
+  position: relative;
+  padding: 0 20px;
 }
 .nav-add-btn{
-    padding: 0 20px;
-    text-decoration: none;
+    height: 32.4px;
+}
+.add-btn {
+  padding: 0 8px;
+  color: white;
+  border-radius: 3px;
 }
 </style>
