28a9e848b046e1711ae2a101757d20141891ab40
Andrew Blair <blair.d.andrew@gmail.com>
2020-07-05 10:20:45 +0100

    created add-face component

---
+++ src/components/AddFace.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>Add Face</h2>
+</template>
+
+<script>
+export default {
+    name: 'AddFace',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddFace from '@/components/AddFace'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-face',
+      name: 'AddFace',
+      component: AddFace
     }
   ]
 })
